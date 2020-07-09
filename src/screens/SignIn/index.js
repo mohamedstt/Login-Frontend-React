79f29083a232ea32285fe63150aaaeb472024071
Mohamed Schuster <mohamedschof@gmail.com>
2020-07-09 15:05:19 -0300

    Sign in with api implemented.

--- src/screens/SignIn/index.js
+++ src/screens/SignIn/index.js
@@ -1,17 +1,22 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link , Redirect} from "react-router-dom";
 import { connect } from "react-redux";
-import { signIn } from './SignInActions';
-
+import { signIn } from "./SignInActions";
 
 const SignIn = (props) => {
-  const { signIn } = props;
+  const { signIn, account } = props;
 
   const submitHandler = (e) => {
     e.preventDefault();
-    signIn({ email: 'mohamedschof@gmail.com', password: '123456' })
+
+    const formData = new FormData(e.target);
+    const data = Object.fromEntries(formData);
+
+    signIn(data);
   };
-  
+  if (account) {
+    return <Redirect to="/manage/links" />;
+  }
   return (
     <div className="container h-100 pt-5">
       <h1>Sign In</h1>
@@ -19,11 +24,15 @@ const SignIn = (props) => {
         <form onSubmit={submitHandler}>
           <div className="form-group">
             <label>Email</label>
-            <input type="text" className="form-control"></input>
+            <input type="text" className="form-control" name="email"></input>
           </div>
           <div className="fprm-group">
             <label>Password</label>
-            <input type="password" className="form-control"></input>
+            <input
+              type="password"
+              className="form-control"
+              name="password"
+            ></input>
           </div>
           <div>
             <button className="btn btn-primary btn-round">Submit</button>
@@ -39,8 +48,7 @@ const SignIn = (props) => {
 };
 
 const mapStateToProps = (state) => {
-  return { account: state.signIn.account }
+  return { account: state.signIn.account };
 };
-
 
 export default connect(mapStateToProps, { signIn })(SignIn);
