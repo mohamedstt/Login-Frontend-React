5a556d73a24749d523f246dbdf3e687b49c410ec
Mohamed Schuster <mohamedschof@gmail.com>
2020-07-15 18:57:58 -0300

    Custom form components.

--- src/screens/Manage/Links/Edit/index.js
+++ src/screens/Manage/Links/Edit/index.js
@@ -1,29 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import Layouts from "../../../Layouts/Manage";
 
-import Layouts from '../../../Layouts/Manage'
+import FormGroup from "../../../../components/FormGroup";
+import FormCheck from "../../../../components/FormCheck";
 
-const Edit = () => {
+import { linkGet } from "../../../../actions/LinkActions";
+import { connect } from "react-redux";
+
+const Edit = ({ link, linkGet }) => {
+  const { id } = useParams();
+
+  useEffect(() => {
+    linkGet(id);
+  }, [id, linkGet]);
+
+  console.log("*** Edit.id", id);
+  console.log("*** Edit.link", link);
+
   return (
     <Layouts>
       <h1>Edit a Link</h1>
       <div>
         <form>
-          <div className="form-group">
-            <label>Label</label>
-            <input type="text" className="form-control"></input>
-          </div>
-          <div className="form-group">
-            <label>Url</label>
-            <input type="text" className="form-control"></input>
-          </div>
+          <FormGroup label="Label" name="label" data={link} type="text" />
+          <FormGroup label="Url" name="url" data={link} type="text" />
+          <FormCheck label="Is Social" name="isSocial" data={link} />
           <div>
-            <div className="form-group form-check">
-              <label className="form-check-label">
-                <input type="checkbox" name="isSocial" />
-                <span className="form-check-sign"></span>
-              isSocial
-            </label>
-            </div>
             <button className="btn btn-primary btn-round">Submit</button>
           </div>
         </form>
@@ -32,4 +35,8 @@ const Edit = () => {
   );
 };
 
-export default Edit;
+const mapStateToProps = (state) => {
+  return { link: state.link.link };
+};
+
+export default connect(mapStateToProps, { linkGet })(Edit);
--- src/screens/Manage/Links/index.js
+++ src/screens/Manage/Links/index.js
@@ -25,7 +25,7 @@ const Links = ({ links, linkList }) => {
       {links && links.length
         ? links.map((link) => {
             return (
-              <div className="pb-2 pt-2 pl-3 pr-3 d-flex flex-row justify-content-between">
+              <div key={link.id} className="pb-2 pt-2 pl-3 pr-3 d-flex flex-row justify-content-between">
                 <div className="pr-3">
                   <img src="http://via.placeholder.com/100" alt="Link icon" />
                 </div>
@@ -34,7 +34,9 @@ const Links = ({ links, linkList }) => {
                   <span className="text-primary clearfix">{link.url}</span>
                 </div>
                 <div className="ml-auto p-2 clearfix">
-                  <span>Edit</span>
+                  <Link to={`/manage/links/edit/${link.id}`}>
+                    <span>Edit</span>
+                  </Link>
                   <span>Delete</span>
                 </div>
               </div>
