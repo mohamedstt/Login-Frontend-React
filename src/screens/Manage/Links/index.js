681fcc9ab93e19006cb240d53a1ced7b40b49ffb
Mohamed Schuster <mohamedschof@gmail.com>
2020-07-15 16:44:04 -0300

    Links Listing.

--- src/screens/Manage/Links/index.js
+++ src/screens/Manage/Links/index.js
@@ -1,36 +1,52 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect } from "react";
+import Layouts from "../../Layouts/Manage";
+import { Link } from "react-router-dom";
+import { linkList } from "../../../actions/LinkActions";
+import { connect } from "react-redux";
 
-import Layouts from '../../Layouts/Manage'
+const Links = ({ links, linkList }) => {
+  useEffect(() => {
+    linkList();
+  }, [linkList]);
 
-const Links = () => {
   return (
     <Layouts>
-    <div className="row">
-      <div className="col">
-        <h1>Links</h1>
-      </div>
-      <div className="col text-right allign-self-bottom pt-2">
-        <Link to="/manage/links/create" className="btn btn-primary">
-          Add
-        </Link>
-      </div>
-      </div>
-      <div className="pb-2 pt-2 pl-3 pr-3 d-flex flex-row justify-content-between">
-        <div className="pr-3">
-          <img src="http://via.placeholder.com/100" alt="Link icon" />
+      <div className="row">
+        <div className="col">
+          <h1>Links</h1>
         </div>
-        <div className="align-self-center">
-          <span className="text-primary clearfix">Item label</span>
-          <span className="text-primary clearfix">Item Url</span>
-        </div>
-        <div className="ml-auto p-2 clearfix">
-          <span>Edit</span>
-          <span>Delete</span>
+        <div className="col text-right allign-self-bottom pt-2">
+          <Link to="/manage/links/create" className="btn btn-primary">
+            Add
+          </Link>
         </div>
       </div>
+
+      {links && links.length
+        ? links.map((link) => {
+            return (
+              <div className="pb-2 pt-2 pl-3 pr-3 d-flex flex-row justify-content-between">
+                <div className="pr-3">
+                  <img src="http://via.placeholder.com/100" alt="Link icon" />
+                </div>
+                <div className="align-self-center">
+                  <span className="text-primary clearfix">{link.label}</span>
+                  <span className="text-primary clearfix">{link.url}</span>
+                </div>
+                <div className="ml-auto p-2 clearfix">
+                  <span>Edit</span>
+                  <span>Delete</span>
+                </div>
+              </div>
+            );
+          })
+        : null}
     </Layouts>
   );
 };
 
-export default Links 
+const mapStateToProps = (state) => {
+  return { links: state.link.links };
+};
+
+export default connect(mapStateToProps, { linkList })(Links);
